d2e614662683563079e2e63844945143d42561bc
GEROMAX <tobita0112@gmail.com>
2018-06-04 07:08:08 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_page/home'
+  get "static_page/home"
 
-  get 'static_page/help'
+  get "static_page/help"
+
+  get "static_page/about"
 
   root "application#live"
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
@@ -11,4 +11,8 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get static_page_about_url
+    assert_response :success
+  end
 end
