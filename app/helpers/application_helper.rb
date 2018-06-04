f031d45c46a7b81f52ba5d952298401284041fd3
GEROMAX <tobita0112@gmail.com>
2018-06-04 17:58:51 +0900

    add helper method for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    return default_title unless page_title.present?
+    "#{page_title} | #{default_title}"
+  end
+
+  private
+
+  def default_title
+    "Ruby on Rails Tutorial Sample App"
+  end
 end
