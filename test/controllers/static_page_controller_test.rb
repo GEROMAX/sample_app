ff7e5219c4d6f651feac99f87edac11cf2ff0615
GEROMAX <tobita0112@gmail.com>
2018-10-03 09:44:31 +0900

    change directory name

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class StaticPageControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
@@ -12,25 +12,25 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_page_home_url
+    get static_pages_home_url
     assert_response :success
     assert_select "title", @base_title
   end
 
   test "should get help" do
-    get static_page_help_url
+    get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_page_about_url
+    get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_page_contact_url
+    get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
